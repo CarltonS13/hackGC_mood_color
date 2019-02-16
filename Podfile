33f7514103fa2c34d0dda41f3fca89f5db23af4a
CarltonS13 <carltonsegbefia@gmail.com>
2019-02-15 23:03:56 -0600

    Switched to cocoa pods and fixed picker

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+use_frameworks!
+
+target 'feels' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  pod 'JTAppleCalendar', '~> 7.0'
+  pod 'ChromaColorPicker'
+
+  # Pods for feels
+
+  target 'feelsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'feelsUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+
+end
--- feels/ViewController.swift
+++ feels/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import ChromaColorPicker
 
 class ViewController: UIViewController {
-
+    
+    var colorPicker: ChromaColorPicker!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        /* Calculate relative size and origin in bounds */
+        let pickerSize = CGSize(width: view.bounds.width*0.8, height: view.bounds.width*0.8)
+        let pickerOrigin = CGPoint(x: view.bounds.midX - pickerSize.width/2, y: view.bounds.midY - pickerSize.height/2)
+        
+        /* Create Color Picker */
+        colorPicker = ChromaColorPicker(frame: CGRect(origin: pickerOrigin, size: pickerSize))
+        
+        /* Customize the view (optional) */
+        colorPicker.padding = 10
+        colorPicker.stroke = 15 //stroke of the rainbow circle
+        colorPicker.currentAngle = Float.pi
+        
+        /* Customize for grayscale (optional) */
+        colorPicker.supportsShadesOfGray = false // false by default
+        //colorPicker.colorToggleButton.grayColorGradientLayer.colors = [UIColor.lightGray.cgColor, UIColor.gray.cgColor] // You can also override gradient colors
+        colorPicker.handleLine.lineWidth = 2.5
+        colorPicker.handleLine.strokeColor = UIColor.lightGray.cgColor
+        colorPicker.handleLine.isHidden = false
+        colorPicker.hexLabel.isHidden = true
+        
+        /* Don't want an element like the shade slider? Just hide it: */
+        //colorPicker.shadeSlider.hidden = true
+        
+        self.view.addSubview(colorPicker)
     }
-
-
+    
+    
 }
 
